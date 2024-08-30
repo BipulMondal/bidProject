cd1fddfaf9afb2fdb283e637a5ff73dfe46afd2d
Bipul Mondal <bipulmondal3007@gmail.com>
2024-08-31 01:48:30 +0530

    commit

--- src/view/reports/AccountStatements.jsx
+++ src/view/reports/AccountStatements.jsx
@@ -1,175 +1,195 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Table from "../../components/Table";
 import { useNavigate } from "react-router-dom";
+import { requestHandler } from "../../utils/requestHandler";
 
 const AccountStatements = () => {
-    const navigate = useNavigate();
-    const filterData = [];
-  
-    const tableData = {
-      columns: [
-        {
-          label: "S.No",
-          field: "S_No",
-          sort: "asc",
-          width: 50,
+  const navigate = useNavigate();
+
+  const fatchAccountStatement = async () => {
+    try {
+      let token = localStorage.getItem("token");
+      let res = await requestHandler({
+        endpoint: "/transaction?page=1&limit=10",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-        {
-          label: "Date",
-          field: "Date",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "Credit",
-          field: "Credit",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "Debit",
-          field: "Debit",
-          sort: "asc",
-          width: 100,
-          height: 100,
-        },
-        {
-          label: "Closing",
-          field: "bst",
-          sort: "asc",
-          width: 100,
-          height: 100,
-        },
-        {
-          label: "Description",
-          field: "Description",
-          sort: "asc",
-          width: 100,
-          height: 100,
-        },
-        
-      ],
-      rows:
-        filterData?.length > 0
-          ? filterData?.map((item, idx) => {
-              return {
-                username: (
-                  <div className="widget-content p-0">
-                    <div className="widget-content-wrapper">
-                      <div
-                        className="widget-content-left flex2"
-                        style={{ textAlign: "center" }}
-                      >
-                        <div className="zwidget-heading" style={{ opacity: 1 }}>
-                          {item.cityName}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ),
-                city_name: (
-                  <div className="widget-content p-0">
-                    <div className="widget-content-wrapper">
-                      <div
-                        className="widget-content-left flex2"
-                        style={{ textAlign: "center" }}
-                      >
-                        <div className="zwidget-heading" style={{ opacity: 1 }}>
-                          {item.cityName}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ),
-                picture: (
-                  <div
-                    className="widget-content p-0"
-                    key={idx}
-                    style={{ textAlign: "center" }}
-                  >
-                    <div className="widget-content-wrapper">
-                      <img
-                        src={`${item.picture}`}
-                        alt=""
-                        style={{
-                          height: "70px",
-                          width: "120px",
-                          marginLeft: "auto",
-                          marginRight: "auto",
-                        }}
-                        className="img-fluid"
-                      />
-                    </div>
-                  </div>
-                ),
-                action: (
-                  <div>
-                    <button className="me-2 btn-icon btn-icon-only btn btn-outline-danger">
-                      <i className="fa-solid fa-trash-can" />
-                    </button>
-  
-                    <button
-                      className="me-2 btn-icon btn-icon-only btn btn-outline-success"
-                      onClick={() => navigate(`/edit-city/${item?._id}`)}
+      });
+      console.log("ddddd", res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fatchAccountStatement();
+  }, []);
+
+  const filterData = [];
+
+  const tableData = {
+    columns: [
+      {
+        label: "S.No",
+        field: "S_No",
+        sort: "asc",
+        width: 50,
+      },
+      {
+        label: "Date",
+        field: "Date",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Credit",
+        field: "Credit",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Debit",
+        field: "Debit",
+        sort: "asc",
+        width: 100,
+        height: 100,
+      },
+      {
+        label: "Closing",
+        field: "bst",
+        sort: "asc",
+        width: 100,
+        height: 100,
+      },
+      {
+        label: "Description",
+        field: "Description",
+        sort: "asc",
+        width: 100,
+        height: 100,
+      },
+    ],
+    rows:
+      filterData?.length > 0
+        ? filterData?.map((item, idx) => {
+            return {
+              username: (
+                <div className="widget-content p-0">
+                  <div className="widget-content-wrapper">
+                    <div
+                      className="widget-content-left flex2"
+                      style={{ textAlign: "center" }}
                     >
-                      <i class="fa-regular fa-pen-to-square"></i>
-                    </button>
+                      <div className="zwidget-heading" style={{ opacity: 1 }}>
+                        {item.cityName}
+                      </div>
+                    </div>
                   </div>
-                ),
-              };
-            })
-          : [],
-    };
+                </div>
+              ),
+              city_name: (
+                <div className="widget-content p-0">
+                  <div className="widget-content-wrapper">
+                    <div
+                      className="widget-content-left flex2"
+                      style={{ textAlign: "center" }}
+                    >
+                      <div className="zwidget-heading" style={{ opacity: 1 }}>
+                        {item.cityName}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ),
+              picture: (
+                <div
+                  className="widget-content p-0"
+                  key={idx}
+                  style={{ textAlign: "center" }}
+                >
+                  <div className="widget-content-wrapper">
+                    <img
+                      src={`${item.picture}`}
+                      alt=""
+                      style={{
+                        height: "70px",
+                        width: "120px",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                      }}
+                      className="img-fluid"
+                    />
+                  </div>
+                </div>
+              ),
+              action: (
+                <div>
+                  <button className="me-2 btn-icon btn-icon-only btn btn-outline-danger">
+                    <i className="fa-solid fa-trash-can" />
+                  </button>
+
+                  <button
+                    className="me-2 btn-icon btn-icon-only btn btn-outline-success"
+                    onClick={() => navigate(`/edit-city/${item?._id}`)}
+                  >
+                    <i class="fa-regular fa-pen-to-square"></i>
+                  </button>
+                </div>
+              ),
+            };
+          })
+        : [],
+  };
   return (
     <>
-<div className="statement_main">
-      <h5> Account Statement</h5>
-      <form>
-        <div className="container-fluid">
-          <div className="row gx-2">  
-            <div class="form-group col-md-2">
-              <label for="inputPassword" class="col-form-label">
-                Search By Client Name
-              </label>
-              <div class="">
-              <select class="form-control" id="exampleFormControlSelect1">
-                  <option selected>Search...</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
+      <div className="statement_main">
+        <h5> Account Statement</h5>
+        <form>
+          <div className="container-fluid">
+            <div className="row gx-2">
+              <div class="form-group col-md-2">
+                <label for="inputPassword" class="col-form-label">
+                  Search By Client Name
+                </label>
+                <div class="">
+                  <select class="form-control" id="exampleFormControlSelect1">
+                    <option selected>Search...</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            {/* From */}
-            <div class="form-group col-md-2">
-              <label for="inputPassword" class="col-form-label">
-                From
-              </label>
-              <div class="">
-                <input type="date" class="form-control" id="inputPassword" />
+              {/* From */}
+              <div class="form-group col-md-2">
+                <label for="inputPassword" class="col-form-label">
+                  From
+                </label>
+                <div class="">
+                  <input type="date" class="form-control" id="inputPassword" />
+                </div>
               </div>
-            </div>
-            {/* To */}
-            <div class="form-group col-md-2">
-              <label for="inputPassword" class="col-form-label">
-                To
-              </label>
-              <div class="">
-                <input type="date" class="form-control" id="inputPassword" />
+              {/* To */}
+              <div class="form-group col-md-2">
+                <label for="inputPassword" class="col-form-label">
+                  To
+                </label>
+                <div class="">
+                  <input type="date" class="form-control" id="inputPassword" />
+                </div>
               </div>
-            </div>
-            <div class="form-group col-md-1 load_button">
-              <button class="btn btn-warning">load</button>
+              <div class="form-group col-md-1 load_button">
+                <button class="btn btn-warning">load</button>
+              </div>
             </div>
           </div>
-        </div>
         </form>
-        </div>
-        <div className="table_div">
-          <Table data={tableData} />
-        </div>
-        </>
-  
+      </div>
+      <div className="table_div">
+        <Table data={tableData} />
+      </div>
+    </>
   );
 };
 
